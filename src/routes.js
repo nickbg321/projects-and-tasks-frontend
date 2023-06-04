9e9fe8ef17d46a4469a1562156eca524e5de02ef
Nikolay Ivanov <nikolay.ivanov321@gmail.com>
2023-06-04 10:08:20 +0300

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+export const GET_PROJECTS_COLLECTION = '/api/projects';
+export const GET_ONE_PROJECT = '/api/projects/{id}';
+export const CREATE_PROJECT = '/api/projects';
+export const UPDATE_PROJECT = '/api/projects/{id}';
+export const DELETE_PROJECT = '/api/projects/{id}';
+export const GET_TASKS_FOR_PROJECT = '/api/projects/{id}/tasks';
+export const CREATE_TASK = '/api/tasks';
+export const UPDATE_TASK = '/api/tasks/{id}';
+export const DELETE_TASK = '/api/tasks/{id}';
